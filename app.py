bf38facf58e605c5b948b93533fae35427764f02
Vishnudas2003 <vishnudas7412@gmail.com>
2026-02-11 10:20:18 -0500

    Created app.py and Index.html

---
+++ app.py
@@ -0,0 +1,15 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+
+
+color = os.environ.get('APP_COLOR')
+
+def main():
+   print(color)
+   return render_template('index.html', color=color)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
